57f8a11652c794383333b618ff9c821ae55f86f1
proninp <proninpaul@yandex.ru>
2025-04-29 17:44:10 +0300

    Minor refactoring

--- src/backend/FinanceManager.Core/FinanceManager.Core.Domain/Entities/Category.cs
+++ src/backend/FinanceManager.Core/FinanceManager.Core.Domain/Entities/Category.cs
@@ -8,17 +8,31 @@ public class Category(
     Guid? parentCategoryId,
     bool income,
     bool expense,
-    bool deleted = false,
-    string emoji = null!) : IdentityEntity
+    bool isDeleted = false,
+    string? emoji = null,
+    byte[]? icon = null) : IdentityEntity
 {
     public string Name { get; set; } = name;
+    
     public Guid UserId { get; set; } = userId;
+    
     public User User { get; set; } = null!;
+    
     public Guid? ParentCategoryId { get; set; } = parentCategoryId;
+    
     public Category? ParentCategory { get; set; } = null!;
+    
     public ICollection<Category> SubCategories { get; set; } = new List<Category>();
+    
     public bool Income { get; set; } = income;
+    
     public bool Expense { get; set; } = expense;
-    public bool Deleted { get; set; } = deleted;
+    
+    public bool IsDeleted { get; set; } = isDeleted;
+    
     public string? Emoji { get; set; } = emoji;
+
+    public byte[]? Icon { get; set; } = icon;
+    
+    public ICollection<Transaction> Transactions { get; set; } = new List<Transaction>();
 }
--- src/backend/FinanceManager.Core/FinanceManager.Core.Domain/Entities/RefreshToken.cs
+++ src/backend/FinanceManager.Core/FinanceManager.Core.Domain/Entities/RefreshToken.cs
@@ -5,8 +5,12 @@ namespace FinanceManager.Core.Domain.Entities;
 public class RefreshToken(Guid userId, string token, DateTime expiresAt, bool isRevoked = false) : IdentityEntity
 {
     public Guid UserId { get; init; } = userId;
+    
     public User User { get; init; } = null!;
+    
     public string Token { get; set; } = token;
+    
     public DateTime ExpiresAt { get; set; } = expiresAt;
+    
     public bool IsRevoked { get; set; } = isRevoked;
 }
