e7e27a332f6e0131e58af4e2424413ca34567430
proninp <proninpaul@yandex.ru>
2025-06-04 21:48:48 +0300

    Added RegistryHolder DTOs

---
+++ src/backend/FinanceManager.CatalogService/FinanceManager.CatalogService.Contracts/DTOs/RegistryHolders/RegistryHolderDto.cs
@@ -0,0 +1,15 @@
+﻿using FinanceManager.CatalogService.Domain.Enums;
+
+namespace FinanceManager.CatalogService.Contracts.DTOs.RegistryHolders;
+
+/// <summary>
+/// DTO для владельца реестра финансовых данных
+/// </summary>
+/// <param name="Id">Уникальный идентификатор владельца</param>
+/// <param name="TelegramId">Идентификатор пользователя в Telegram</param>
+/// <param name="Role">Роль пользователя в системе</param>
+public record RegistryHolderDto(
+    Guid Id,
+    long TelegramId,
+    Role Role
+);
---
+++ src/backend/FinanceManager.CatalogService/FinanceManager.CatalogService.Contracts/DTOs/RegistryHolders/RegistryHolderFilterDto.cs
@@ -0,0 +1,17 @@
+﻿using FinanceManager.CatalogService.Domain.Enums;
+
+namespace FinanceManager.CatalogService.Contracts.DTOs.RegistryHolders;
+
+/// <summary>
+/// DTO для фильтрации и пагинации владельцев реестра
+/// </summary>
+/// <param name="ItemsPerPage">Количество элементов на странице</param>
+/// <param name="Page">Номер страницы</param>
+/// <param name="TelegramId">Идентификатор пользователя в Telegram</param>
+/// <param name="Role">Роль пользователя в системе</param>
+public record RegistryHolderFilterDto(
+    int ItemsPerPage,
+    int Page,
+    long? TelegramId,
+    Role? Role
+);
