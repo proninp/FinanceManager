85dcb13c6c921c32eae188cb2103be1fc14b22d3
proninp <proninpaul@yandex.ru>
2025-06-05 08:54:05 +0300

    Added ExchangeRate DTOs

---
+++ src/backend/FinanceManager.CatalogService/FinanceManager.CatalogService.Contracts/DTOs/ExchangeRates/ExchangeRateFilterDto.cs
@@ -0,0 +1,17 @@
+﻿namespace FinanceManager.CatalogService.Contracts.DTOs.ExchangeRates;
+
+/// <summary>
+/// DTO для фильтрации и пагинации списка обменных курсов
+/// </summary>
+/// <param name="ItemsPerPage">Количество элементов на странице</param>
+/// <param name="Page">Номер страницы</param>
+/// <param name="CurrencyId">Идентификатор валюты</param>
+/// <param name="RateDate">Дата курса</param>
+/// <param name="Rate">Значение курса</param>
+public record ExchangeRateFilterDto(
+    int ItemsPerPage,
+    int Page,
+    Guid? CurrencyId,
+    DateTime? RateDate,
+    decimal? Rate
+);
---
+++ src/backend/FinanceManager.CatalogService/FinanceManager.CatalogService.Contracts/DTOs/ExchangeRates/UpdateExchangeRateDto.cs
@@ -0,0 +1,15 @@
+﻿namespace FinanceManager.CatalogService.Contracts.DTOs.ExchangeRates;
+
+/// <summary>
+/// DTO для обновления обменного курса валюты
+/// </summary>
+/// <param name="Id">Идентификатор курса</param>
+/// <param name="RateDate">Дата курса</param>
+/// <param name="CurrencyId">Идентификатор валюты</param>
+/// <param name="Rate">Значение курса</param>
+public record UpdateExchangeRateDto(
+    Guid Id,
+    DateTime? RateDate,
+    Guid? CurrencyId,
+    decimal? Rate
+);
