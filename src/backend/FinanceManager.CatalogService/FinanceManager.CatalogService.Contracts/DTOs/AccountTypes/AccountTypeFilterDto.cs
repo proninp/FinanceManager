f4cd04edab2f684277f37622f277528509ab5818
proninp <proninpaul@yandex.ru>
2025-06-05 08:20:45 +0300

    Added AccountType DTOs

---
+++ src/backend/FinanceManager.CatalogService/FinanceManager.CatalogService.Contracts/DTOs/AccountTypes/AccountTypeFilterDto.cs
@@ -0,0 +1,15 @@
+﻿namespace FinanceManager.CatalogService.Contracts.DTOs.AccountTypes;
+
+/// <summary>
+/// DTO для фильтрации и пагинации типов счетов
+/// </summary>
+/// <param name="ItemsPerPage">Количество элементов на странице</param>
+/// <param name="Page">Номер страницы</param>
+/// <param name="Code">Код типа счета</param>
+/// <param name="Description">Описание типа счета</param>
+public record AccountTypeFilterDto(
+    int ItemsPerPage,
+    int Page,
+    string? Code,
+    string? Description
+);
