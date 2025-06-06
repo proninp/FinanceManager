52ab8fd828e18682a76b8068e09586481898ae59
proninp <proninpaul@yandex.ru>
2025-06-06 18:45:00 +0300

    Added Country, Currency, ExchangeRate and RegistryHolder repositories

---
+++ src/backend/FinanceManager.CatalogService/FinanceManager.CatalogService.Abstractions/Repositories/ICountryRepository.cs
@@ -0,0 +1,34 @@
+﻿using FinanceManager.CatalogService.Abstractions.Repositories.Common;
+using FinanceManager.CatalogService.Contracts.DTOs.Countries;
+using FinanceManager.CatalogService.Domain.Entities;
+
+namespace FinanceManager.CatalogService.Abstractions.Repositories;
+
+public interface ICountryRepository : IBaseRepository<Country, CountryFilterDto>, IInitializerRepository<Country>
+{
+    /// <summary>
+    /// Получает все страны, отсортированные по названию
+    /// </summary>
+    /// <param name="ascending">Направление сортировки (по возрастанию по умолчанию)</param>
+    /// <param name="cancellationToken">Токен отмены операции</param>
+    /// <returns>Список всех стран, отсортированный по названию</returns>
+    Task<IEnumerable<Country>> GetAllOrderedByNameAsync(bool ascending = true,
+        CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Проверяет уникальность названия страны
+    /// </summary>
+    /// <param name="name">Название страны</param>
+    /// <param name="excludeId">Исключить страну с данным ID (для обновления)</param>
+    /// <param name="cancellationToken">Токен отмены операции</param>
+    /// <returns>True, если название уникально</returns>
+    Task<bool> IsNameUniqueAsync(string name, Guid? excludeId = null, CancellationToken cancellationToken = default);
+    
+    /// <summary>
+    /// Проверяет, можно ли удалить страну (не используется ли она в банках или валютах)
+    /// </summary>
+    /// <param name="countryId">Идентификатор страны</param>
+    /// <param name="cancellationToken">Токен отмены операции</param>
+    /// <returns>True, если страну можно удалить</returns>
+    Task<bool> CanBeDeletedAsync(Guid countryId, CancellationToken cancellationToken = default);
+}
---
+++ src/backend/FinanceManager.CatalogService/FinanceManager.CatalogService.Abstractions/Repositories/IExchangeRateRepository.cs
@@ -0,0 +1,57 @@
+﻿using FinanceManager.CatalogService.Abstractions.Repositories.Common;
+using FinanceManager.CatalogService.Contracts.DTOs.ExchangeRates;
+using FinanceManager.CatalogService.Domain.Entities;
+
+namespace FinanceManager.CatalogService.Abstractions.Repositories;
+
+/// <summary>
+/// Интерфейс репозитория для работы с обменными курсами валют
+/// </summary>
+public interface IExchangeRateRepository : IBaseRepository<ExchangeRate, ExchangeRateFilterDto>
+{
+    /// <summary>
+    /// Проверяет существование курса для валюты на дату
+    /// </summary>
+    /// <param name="currencyId">Идентификатор валюты</param>
+    /// <param name="rateDate">Дата курса</param>
+    /// <param name="cancellationToken">Токен отмены операции</param>
+    /// <returns>True, если курс существует</returns>
+    Task<bool> ExistsForCurrencyAndDateAsync(Guid currencyId, DateTime rateDate,
+        CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Проверяет, есть ли курсы на указанную дату
+    /// </summary>
+    /// <param name="rateDate">Дата курсов</param>
+    /// <param name="cancellationToken">Токен отмены операции</param>
+    /// <returns>True, если курсы на дату существуют</returns>
+    Task<bool> ExistsForDateAsync(DateTime rateDate, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Получает дату последнего курса валюты
+    /// </summary>
+    /// <param name="currencyId">Идентификатор валюты</param>
+    /// <param name="cancellationToken">Токен отмены операции</param>
+    /// <returns>Дата последнего курса или null</returns>
+    Task<DateTime?> GetLastRateDateAsync(Guid currencyId, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Добавляет несколько курсов валют
+    /// </summary>
+    /// <param name="exchangeRates">Список курсов для добавления</param>
+    /// <param name="cancellationToken">Токен отмены операции</param>
+    /// <returns>Список добавленных курсов</returns>
+    Task<IEnumerable<ExchangeRate>> AddRangeAsync(IEnumerable<ExchangeRate> exchangeRates,
+        CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Удаляет курсы валюты за период
+    /// </summary>
+    /// <param name="currencyId">Идентификатор валюты</param>
+    /// <param name="dateFrom">Дата начала периода</param>
+    /// <param name="dateTo">Дата окончания периода</param>
+    /// <param name="cancellationToken">Токен отмены операции</param>
+    /// <returns>Количество удаленных курсов</returns>
+    Task<int> DeleteByPeriodAsync(Guid currencyId, DateTime dateFrom, DateTime dateTo,
+        CancellationToken cancellationToken = default);
+}
