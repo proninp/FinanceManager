1e9ed35d1469d752287c92a15cd2b9d039f5fa7e
proninp <proninpaul@yandex.ru>
2025-06-17 09:04:46 +0300

    Added CurrencyErrorsFactory

---
+++ src/backend/FinanceManager.CatalogService/FinanceManager.CatalogService.Implementations/Errors/Abstractions/ICurrencyErrorsFactory.cs
@@ -0,0 +1,55 @@
+﻿using FluentResults;
+
+namespace FinanceManager.CatalogService.Implementations.Errors.Abstractions;
+
+/// <summary>
+/// Интерфейс фабрики ошибок, связанных с сущностью "Currency"
+/// </summary>
+public interface ICurrencyErrorsFactory
+{
+    /// <summary>
+    /// Создаёт ошибку, указывающую на то, что валюта с указанным идентификатором не найдена
+    /// </summary>
+    /// <param name="id">Идентификатор валюты</param>
+    /// <returns>Экземпляр ошибки</returns>
+    IError NotFound(Guid id);
+
+    /// <summary>
+    /// Создаёт ошибку, указывающую на то, что валюта с указанным кодом валюты уже существует
+    /// </summary>
+    /// <param name="charCode">Код валюты</param>
+    /// <returns>Экземпляр ошибки</returns>
+    IError CharCodeAlreadyExists(string charCode);
+    
+    /// <summary>
+    /// Создаёт ошибку, указывающую на то, что валюта с указанным числовым кодом уже существует
+    /// </summary>
+    /// <param name="numCode">Числовой код валюты</param>
+    /// <returns>Экземпляр ошибки</returns>
+    IError NumCodeAlreadyExists(string numCode);
+
+    /// <summary>
+    /// Создаёт ошибку, указывающую на обязательность заполнения кода валюты
+    /// </summary>
+    /// <returns>Экземпляр ошибки</returns>
+    IError CharCodeIsRequired();
+    
+    /// <summary>
+    /// Создаёт ошибку, указывающую на обязательность заполнения числового кода валюты
+    /// </summary>
+    /// <returns>Экземпляр ошибки</returns>
+    IError NumCodeIsRequired();
+    
+    /// <summary>
+    /// Создаёт ошибку, указывающую на обязательность заполнения наименования валюты
+    /// </summary>
+    /// <returns>Экземпляр ошибки</returns>
+    IError NameIsRequired();
+
+    /// <summary>
+    /// Создаёт ошибку, указывающую на невозможность удаления валюты, если она используется в других сущностях
+    /// </summary>
+    /// <param name="id">Идентификатор валюты</param>
+    /// <returns>Экземпляр ошибки</returns>
+    IError CannotDeleteUsedCurrency(Guid id);
+}
---
+++ src/backend/FinanceManager.CatalogService/FinanceManager.CatalogService.Implementations/Errors/CurrencyErrorsFactory.cs
@@ -0,0 +1,67 @@
+﻿using FinanceManager.CatalogService.Implementations.Errors.Abstractions;
+using FluentResults;
+
+namespace FinanceManager.CatalogService.Implementations.Errors;
+
+/// <summary>
+/// Фабрика ошибок для сущности Currency
+/// </summary>
+public class CurrencyErrorsFactory(IErrorsFactory errorsFactory) : ICurrencyErrorsFactory
+{
+    private const string EntityName = "Currency";
+    private const string CharCodeField = "CharCode";
+    private const string NumCodeField = "NumCode";
+    
+    /// <summary>
+    /// Создаёт ошибку, указывающую на то, что валюта с указанным идентификатором не найдена
+    /// </summary>
+    /// <param name="id">Идентификатор валюты</param>
+    /// <returns>Экземпляр ошибки</returns>
+    public IError NotFound(Guid id) =>
+        errorsFactory.NotFound("CURRENCY_NOT_FOUND", EntityName, id);
+    
+    /// <summary>
+    /// Создаёт ошибку, указывающую на то, что валюта с указанным символьным кодом уже существует
+    /// </summary>
+    /// <param name="charCode">Код валюты</param>
+    /// <returns>Экземпляр ошибки</returns>
+    public IError CharCodeAlreadyExists(string charCode) =>
+        errorsFactory.AlreadyExists("CURRENCY_CHARCODE_EXISTS", EntityName, CharCodeField, charCode);
+
+    /// <summary>
+    /// Создаёт ошибку, указывающую на то, что валюта с указанным числовым кодом уже существует
+    /// </summary>
+    /// <param name="numCode">Числовой код валюты</param>
+    /// <returns>Экземпляр ошибки</returns>
+    public IError NumCodeAlreadyExists(string numCode) =>
+        errorsFactory.AlreadyExists("CURRENCY_NUMCODE_EXISTS", EntityName, NumCodeField, numCode);
+
+    /// <summary>
+    /// Создаёт ошибку, указывающую на обязательность заполнения символьного кода валюты
+    /// </summary>
+    /// <returns>Экземпляр ошибки</returns>
+    public IError CharCodeIsRequired() =>
+        errorsFactory.Required("CURRENCY_CHARCODE_REQUIRED", EntityName, CharCodeField);
+
+    /// <summary>
+    /// Создаёт ошибку, указывающую на обязательность заполнения числового кода валюты
+    /// </summary>
+    /// <returns>Экземпляр ошибки</returns>
+    public IError NumCodeIsRequired() =>
+        errorsFactory.Required("CURRENCY_NUMCODE_REQUIRED", EntityName, NumCodeField);
+
+    /// <summary>
+    /// Создаёт ошибку, указывающую на обязательность заполнения наименования валюты
+    /// </summary>
+    /// <returns>Экземпляр ошибки</returns>
+    public IError NameIsRequired() =>
+        errorsFactory.Required("CURRENCY_NAME_REQUIRED", EntityName, "Name");
+
+    /// <summary>
+    /// Создаёт ошибку, указывающую на невозможность удаления валюты, если она используется в других сущностях
+    /// </summary>
+    /// <param name="id">Идентификатор валюты</param>
+    /// <returns>Экземпляр ошибки</returns>
+    public IError CannotDeleteUsedCurrency(Guid id) =>
+        errorsFactory.CannotDeleteUsedEntity("CURRENCY_IN_USE", EntityName, id);
+}
